eacab35ef61b175a2f5aad73fa50a06bcd281151
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-10 10:45:29 +0330

    step three
    writing all expectations from our main component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 const App = () => {
 
-  const {data, loading, errorMsg} = Usefetch('')
+  const {data, loading, errorMsg} = Usefetch('https://dummyjson.com/products')
 
   return (
     <div>
       <h1>UseFetch Hook</h1>
+      {loading ? <div>data is loading please wait</div> : null}
+      {errorMsg ? <div>error {errorMsg}</div> : null}
+      {data && data.products && data.products.length ?
+          data.map((item, index) => (
+          <p key={index}>{item.title}</p>
+        ))
+      : null}
     </div>
   )
 }
