ef37f1b74a5ce58712b367c191e1edc7a350bc45
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-10 10:59:38 +0330

    last step
    styling, writing all functionality (our custom hook)

--- vite-project/CustomUseFetch.jsx
+++ vite-project/CustomUseFetch.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function UseFetch () {
+export default function UseFetch (url) {
 
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [errorMsg, setErrorMsg] = useState(null)
+
+    const fetchData = async () => {
+        try {
+            setLoading(true)
+            const response = await fetch(url)
+            const data = await response.json()
+            if(data) {
+                setData(data)
+                setLoading(false)}
+        } catch (e) {
+                setErrorMsg(e)
+                setLoading(false)
+        }
+    }
+    useEffect(()=> {
+    fetchData()
+    }, [url])
     
-
     return {data, loading, errorMsg}
 }
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,12 +6,12 @@ const App = () => {
   const {data, loading, errorMsg} = UseFetch('https://dummyjson.com/products')
 
   return (
-    <div>
-      <h1>UseFetch Hook</h1>
+    <div className='items-center justify-center flex flex-col gap-2'>
+      <h1 className='font-bold text-3xl text-gray-900 mt-4'>UseFetch Hook</h1>
       {loading ? <div>data is loading please wait</div> : null}
       {errorMsg ? <div>error {errorMsg}</div> : null}
       {data && data.products && data.products.length ?
-          data.map((item, index) => (
+          data.products.map((item, index) => (
           <p key={index}>{item.title}</p>
         ))
       : null}
